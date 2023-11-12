d85c9771ec735e4b31bca01aeda2769e7ef24408
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-12 23:48:08 +0700

    Meengembalikan nilai split, reverse dan join
    Meengembalikan nilai split, reverse dan join

---
+++ Tugas_balik_kata.js
@@ -0,0 +1,12 @@
+function target(nilai) {
+    var nilai;
+    const semua = nilai.split('').reverse().join('');
+    for (i = 0 ; i <= semua.length; i++) {
+        console.log("");
+        return semua
+    }
+}
+console.log(target("Niomic!"));
+console.log(target("JavaScript"));
+console.log(target("alohahola"));
+console.log(target("Jawa_Barat"));
